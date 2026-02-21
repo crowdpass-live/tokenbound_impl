46d43e0b1e0ea3c25f474c3b95e90ac13eeee73b
dinahmaccodes <dinahmaccodes@gmail.com>
2026-02-21 03:08:55 +0100

    test: add tests for TicketNft with constructor pattern and get_minter

--- soroban-contract/contracts/ticket_nft/src/test.rs
+++ soroban-contract/contracts/ticket_nft/src/test.rs
@@ -2,21 +2,19 @@
 
 use super::*;
 use soroban_sdk::testutils::Address as _;
-use soroban_sdk::{Env};
+use soroban_sdk::Env;
 
 #[test]
 fn test_minting() {
     let env = Env::default();
     env.mock_all_auths();
 
-    let contract_id = env.register_contract(None, TicketNFT);
-    let client = TicketNFTClient::new(&env, &contract_id);
-
     let minter = Address::generate(&env);
     let user1 = Address::generate(&env);
     let user2 = Address::generate(&env);
 
-    client.initialize(&minter);
+    let contract_id = env.register(TicketNft, (&minter,));
+    let client = TicketNftClient::new(&env, &contract_id);
 
     // Mint first ticket
     let token_id1 = client.mint_ticket_nft(&user1);
@@ -32,18 +30,16 @@ fn test_minting() {
 }
 
 #[test]
-#[should_panic(expected = "User already has ticket")]
+#[should_panic(expected = "User already has a ticket")]
 fn test_cannot_mint_twice_to_same_user() {
     let env = Env::default();
     env.mock_all_auths();
 
-    let contract_id = env.register_contract(None, TicketNFT);
-    let client = TicketNFTClient::new(&env, &contract_id);
-
     let minter = Address::generate(&env);
     let user = Address::generate(&env);
 
-    client.initialize(&minter);
+    let contract_id = env.register(TicketNft, (&minter,));
+    let client = TicketNftClient::new(&env, &contract_id);
 
     client.mint_ticket_nft(&user);
     client.mint_ticket_nft(&user); // Should panic
@@ -54,17 +50,15 @@ fn test_transfer() {
     let env = Env::default();
     env.mock_all_auths();
 
-    let contract_id = env.register_contract(None, TicketNFT);
-    let client = TicketNFTClient::new(&env, &contract_id);
-
     let minter = Address::generate(&env);
     let user1 = Address::generate(&env);
     let user2 = Address::generate(&env);
 
-    client.initialize(&minter);
+    let contract_id = env.register(TicketNft, (&minter,));
+    let client = TicketNftClient::new(&env, &contract_id);
 
     let token_id = client.mint_ticket_nft(&user1);
-    
+
     client.transfer_from(&user1, &user2, &token_id);
 
     assert_eq!(client.owner_of(&token_id), user2);
@@ -78,14 +72,12 @@ fn test_cannot_transfer_to_user_with_ticket() {
     let env = Env::default();
     env.mock_all_auths();
 
-    let contract_id = env.register_contract(None, TicketNFT);
-    let client = TicketNFTClient::new(&env, &contract_id);
-
     let minter = Address::generate(&env);
     let user1 = Address::generate(&env);
     let user2 = Address::generate(&env);
 
-    client.initialize(&minter);
+    let contract_id = env.register(TicketNft, (&minter,));
+    let client = TicketNftClient::new(&env, &contract_id);
 
     let token_id1 = client.mint_ticket_nft(&user1);
     let _token_id2 = client.mint_ticket_nft(&user2);
@@ -99,20 +91,25 @@ fn test_only_minter_can_mint() {
     let env = Env::default();
     // env.mock_all_auths(); // Don't mock auth to test failure
 
-    let contract_id = env.register_contract(None, TicketNFT);
-    let client = TicketNFTClient::new(&env, &contract_id);
+    let minter = Address::generate(&env);
+    let user = Address::generate(&env);
+
+    let contract_id = env.register(TicketNft, (&minter,));
+    let client = TicketNftClient::new(&env, &contract_id);
+
+    // Without mock_all_auths, require_auth() will fail for the minter
+    client.mint_ticket_nft(&user);
+}
+
+#[test]
+fn test_get_minter() {
+    let env = Env::default();
+    env.mock_all_auths();
 
     let minter = Address::generate(&env);
-    let _user = Address::generate(&env);
-    let _non_minter = Address::generate(&env);
 
-    client.initialize(&minter);
+    let contract_id = env.register(TicketNft, (&minter,));
+    let client = TicketNftClient::new(&env, &contract_id);
 
-    // This should fail because we're not calling as minter and haven't mocked auths
-    // To be more precise, we can use env.set_auths or mock_all_auths and then check if require_auth was called.
-    // In Soroban tests, mock_all_auth() makes it so any require_auth() succeeds.
-    // To test failures, we can either not mock, or selectively mock.
-    
-    // If we don't mock, it should fail.
-    client.mint_ticket_nft(&user); 
+    assert_eq!(client.get_minter(), minter);
 }
